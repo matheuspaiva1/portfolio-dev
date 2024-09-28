69e815521b5c61b6e01a2150cce8a8f60bb8e86b
matheuspaiva1 <matheuspaiva0007@gmail.com>
2024-09-27 22:45:04 -0300

    create project

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable @next/next/no-img-element */
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+      <img src="/devindev-light.svg" alt="logo" />
+      <button> <img src="/moon.svg" alt="moon" /> </button>
+
+    </header>
+  )
+}
