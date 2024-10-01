5c60e7b8259d25fcf8f5b5cff21e422295136ec3
matheuspaiva1 <matheuspaiva0007@gmail.com>
2024-10-01 15:03:51 -0300

    feat: layout footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,9 +1,22 @@
+/* eslint-disable @next/next/no-img-element */
 import React from 'react'
+import { FaWhatsapp } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+
 
 export default function Footer() {
+  
   return (
     <footer>
-      
+      <h1 className="text-center text-xl my-4">Matheus Paiva</h1>
+      <div className="flex items-center justify-center gap-8 my-4">
+        <FaWhatsapp className='w-8 h-8'/>
+        <FaInstagram className='w-8 h-8'/>
+        <FaGithub className='w-8 h-8'/>
+        <FaLinkedin className='w-8 h-8'/>
+      </div>
     </footer>
   )
 }
