362c6406674fc7a2f50bcbf603f90e9cdecbbddd
matheuspaiva1 <matheuspaiva0007@gmail.com>
2024-10-01 14:10:29 -0300

    feat: layout page skills

--- src/app/components/CardSkills.tsx
+++ src/app/components/CardSkills.tsx
@@ -15,13 +15,13 @@ const CardSkills:React.FC<CardProps>=({title, description, imgs}) => {
     <div className='my-8 m-4'>
       {colorMode === "dark" ? 
       <div className="mt-4 bg-gray-900 p-4 rounded-lg">
-        <img src={imgs} alt="" />
+        <img src={imgs} alt="" className='py-4' />
         <h1 className='font-bold text-2xl'>{title}</h1>
         <p className=''>{description}</p>
       </div> : 
-      <div className="">
-        <img src={imgs} alt="" />
-        <h1>{title}</h1>
+      <div className="mt-4 bg-gray-200 p-4 rounded-lg">
+        <img src={imgs} alt="" className='py-4'/>
+        <h1 className='font-bold text-2xl'>{title}</h1>
         <p>{description}</p>
       </div> 
       }
