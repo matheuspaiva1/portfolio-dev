f4bc8ef99f68a33c0c357980180efebe52c581ec
matheuspaiva1 <matheuspaiva0007@gmail.com>
2024-10-03 15:51:17 -0300

    refactor: refatoração da barra de navegação

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,40 +1,27 @@
 /* eslint-disable @next/next/no-img-element */
 'use client'
-import React, { useState } from 'react'
+import React from 'react'
 import ButtonTheme from './ButtonTheme'
-import { IoMenuOutline } from "react-icons/io5";
 import { useColorMode } from '@chakra-ui/react';
-import ModalNav from './ModalNav';
+import NavBar from './NavBar';
 
 export default function Header() {
   const {colorMode} = useColorMode()
-  const [showModal, setShowModal] = useState(false)
-
-  const openModal = () => setShowModal(true);
-  const closeModal = () => setShowModal(false);
 
   return (
     <header className="mt-4">
       <div className='flex items-center justify-center'>
-      {colorMode === "light" ? <img src="/devindev-light.svg" alt="logo" className='mr-12'/> : <img src="/devindev-dark.svg" alt="logo" className='mr-12'/>}
+      {colorMode === "light" ? <img src="/devindev-light.svg" alt="logo" className='mr-12 ml-6'/> : <img src="/devindev-dark.svg" alt="logo" className='mr-12 ml-6'/>}
       <ButtonTheme/>
-      <div className="mx-24 text-xl flex items-center justify-center gap-10">
-          <a href="#sobre-mim"><h1>Sobre mim</h1></a>
-          <a href="#page-project"><h1>Meus Projetos</h1></a>
-          <a href="#skills"><h1>Habilidades</h1></a>
-          <a href="#contato"><h1>Contato</h1></a>
-        </div>
-      <div className="ml-20">
-        <button>
-          <IoMenuOutline className="w-8 h-8" onClick={openModal}/>
-        </button>
+      
+      <div className="ml-8">
+        <NavBar/>
       </div>
         
       </div>
 
       
 
-      <ModalNav show={showModal} close={closeModal}/>
     </header>
     
   )
---
+++ src/app/components/NavBar.tsx
@@ -0,0 +1,83 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useState } from 'react';
+import {
+  Box,
+  Flex,
+  IconButton,
+  Drawer,
+  DrawerBody,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  useDisclosure,
+  Button,
+  Stack,
+} from '@chakra-ui/react';
+import { IoMenuOutline } from "react-icons/io5";
+import Link from 'next/link'; // Importando o Link do next
+
+
+const NavBar = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [isMobile] = useState(() => window.innerWidth < 768);
+
+  const navItems = [
+    { label: 'Sobre mim', href: '#sobre-mim' },
+    { label: 'Projetos', href: '#page-project' },
+    { label: 'Habilidades', href: '#skills' },
+    { label: 'Contato', href: '#contato' },
+  ];
+
+  return (
+    <Box>
+      <Flex
+        as="nav"
+        color="white"
+        padding="1.5rem"
+        justify="space-between"
+        align="center"
+      >
+        {/* Para telas maiores */}
+        {isMobile === false ?
+          <Flex display={{ base: 'none', md: 'flex' }}>
+            {navItems.map((item) => (
+              <Link key={item.href} href={item.href} passHref>
+                <Button as="a" variant="ghost">
+                  {item.label}
+                </Button>
+            </Link>
+            ))}
+          </Flex> :
+          <IconButton
+            aria-label="Open menu"
+            icon={<IoMenuOutline />}
+            onClick={onOpen}
+          />
+        }
+        {/* Para telas menores */}
+        
+      </Flex>
+
+      {/* Drawer para navegação móvel */}
+      <Drawer isOpen={isOpen} placement="right" onClose={onClose}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerBody>
+            <Stack spacing={4} mt={8}>
+              {navItems.map((item) => (
+                <Link key={item.href} href={item.href} passHref>
+                  <Button as="a" variant="ghost" onClick={onClose}>
+                    {item.label}
+                  </Button>
+                </Link>
+              ))}
+            </Stack>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </Box>
+  );
+};
+
+export default NavBar;
